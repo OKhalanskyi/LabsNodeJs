40a68fe35c93093d16a28ff0304c6e3afa28b436
Oleg Khalanskyi <okhalanskyi@gmail.com>
2022-10-16 20:22:34 +0300

    dotenv dep added

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 import http  from "http";
+import dotenv from "dotenv"
 
-const port = 4308
+dotenv.config({path:".env.dev"})
+const {PORT} = process.env
 const server = http.createServer((req, res)=>{
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end(`started`)
-    console.log('started')
+    console.log(`started on ${PORT}`)
 })
-server.listen(port)
+server.listen(PORT||7777)
