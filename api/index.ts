a9bc6803bb861af830afa7dd03a51328efb34969
Oleg Khalanskyi <okhalanskyi@gmail.com>
2022-10-16 20:45:30 +0300

    cache --force cleaned

--- api/index.ts
+++ api/index.ts
@@ -6,6 +6,6 @@ const {PORT} = process.env
 const server = http.createServer((req, res)=>{
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end(`started`)
-    console.log(`started on ${PORT}`)
+    console.log(`started on ${PORT} , cache cleaned`)
 })
 server.listen(PORT||7777)
