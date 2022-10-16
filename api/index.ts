eae68b0c9ebd8f6c746a4d814b9924e104da7d78
Oleg Khalanskyi <okhalanskyi@gmail.com>
2022-10-17 01:38:39 +0300

    prettier added

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,13 @@
-import http  from "http";
-import dotenv from "dotenv";
-dotenv.config({path:".env.dev"});
-const {PORT} = process.env;
-const server =http.createServer((req, res)=>{
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("started");
-    console.log(`started on ${PORT} , cache cleaned`);
-});
-server.listen(PORT||7777);
+import http from 'http'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env.dev' })
+const { PORT } = process.env
+const server = http.createServer(
+    (req: http.IncomingMessage, res: http.ServerResponse) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end('started')
+        console.log(`started on ${PORT} , cache cleaned`)
+    }
+)
+server.listen(PORT || 7777)
