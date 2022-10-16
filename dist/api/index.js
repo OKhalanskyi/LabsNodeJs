a7ab44a39134a4aaab52cb1c972c3b5a8a3f7f0f
Oleg Khalanskyi <okhalanskyi@gmail.com>
2022-10-16 20:35:57 +0300

    dist added

---
+++ dist/api/index.js
@@ -0,0 +1,11 @@
+import http from "http";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.dev" });
+const { PORT } = process.env;
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`started`);
+    console.log(`started on ${PORT} jsjsjsjs`);
+});
+server.listen(PORT || 7777);
+//# sourceMappingURL=index.js.map
