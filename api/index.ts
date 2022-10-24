6fb200e895691352681a91eee8b9c74009a1db90
Oleg Khalanskyi <okhalanskyi@gmail.com>
2022-10-24 17:59:18 +0300

    eslint-config-prettier added

--- api/index.ts
+++ api/index.ts
@@ -4,9 +4,9 @@ import dotenv from 'dotenv'
 dotenv.config({ path: '.env.dev' })
 const { PORT } = process.env
 const server = http.createServer(
-    (req: http.IncomingMessage, res: http.ServerResponse) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.end('started zxc 0808')
-    }
+  (req: http.IncomingMessage, res: http.ServerResponse) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('started zxc 0808')
+  }
 )
 server.listen(PORT || 7777)
